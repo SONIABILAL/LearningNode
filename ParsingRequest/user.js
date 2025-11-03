c462a729b6e2f94bbdcd895594182ab9ade473d6
soniabilal <soniabilal131358@gmail.com>
2025-11-03 13:08:32 +0500

    learning Async code and parsing request in node.js

--- ParsingRequest/user.js
+++ ParsingRequest/user.js
@@ -1,8 +1,7 @@
-const http = require('http');
 const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
+const userRequestHandler = (req, res) => {
+  console.log(req.url, req.method);
 
   if (req.url === '/') {
     res.setHeader('Content-Type', 'text/html');
@@ -23,7 +22,25 @@ const server = http.createServer((req, res) => {
 
   } else if (req.url.toLowerCase() === "/submit-details" &&
         req.method == "POST") {
-    fs.writeFileSync('user.txt', 'Prashant Jain');
+    
+    const body = [];      
+    req.on('data', chunk => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on('end', () => {
+      const fullBody = Buffer.concat(body).toString();
+      console.log(fullBody);
+      const params = new URLSearchParams(fullBody);
+      // const bodyObject = {};
+      // for (const [key, val] of params.entries()) {
+      //   bodyObject[key] = val;
+      // }
+      const bodyObject = Object.fromEntries(params);
+      console.log(bodyObject);
+      fs.writeFileSync('user.txt', JSON.stringify(bodyObject));
+    });
+
     res.statusCode = 302;
     res.setHeader('Location', '/');
   }
@@ -33,9 +50,6 @@ const server = http.createServer((req, res) => {
   res.write('<body><h1>Like / Share / Subscribe</h1></body>');
   res.write('</html>');
   res.end();
-});
+};
 
-const PORT = 3001;
-server.listen(PORT, () => {
-  console.log(`Server running on address http://localhost:${PORT}`);
-});
+module.exports = userRequestHandler;
